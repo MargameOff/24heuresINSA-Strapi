7b8ea6899f5e730d3b3b9502fb0be0128513603e
Marius <contact@mariusdeleuil.fr>
2024-12-18 10:33:20 +0100

    securisation des artistes

--- src/api/artiste/controllers/artiste.ts
+++ src/api/artiste/controllers/artiste.ts
@@ -2,6 +2,26 @@
  * artiste controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::artiste.artiste');
+export default factories.createCoreController('api::artiste.artiste', ({ strapi }) => ({
+  async find(ctx) {
+    // S'assurer que ctx.query.filters est un objet avec assertion de type
+    const filters = (ctx.query?.filters || {}) as Record<string, any>;
+
+    // Ajouter le filtre pour les artistes publiés
+    ctx.query = {
+      ...ctx.query,
+      filters: {
+        ...filters,
+        dateDePublication: {
+          $lte: new Date().toISOString(),
+        },
+      },
+    };
+
+    // Appeler la méthode find par défaut
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
